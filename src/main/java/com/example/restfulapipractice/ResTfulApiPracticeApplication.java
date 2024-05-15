87965e76f8cb989700e9f3be930e1a531697c2b2
이희수 <hslee9234@naver.com>
2024-05-15 17:54:39 +0900

    first commit

---
+++ src/main/java/com/example/restfulapipractice/ResTfulApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiPracticeApplication.class, args);
+	}
+
+}
